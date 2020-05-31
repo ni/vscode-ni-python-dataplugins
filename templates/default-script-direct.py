be4c44204eb78dc7bc6c0fe1b0629f217207be5e
Jan Schummers <jan.schummers@ni.com>
2020-05-31 09:59:27 +0200

    Updated indirect plugin template

--- templates/default-script-direct.py
+++ templates/default-script-direct.py
@@ -6,9 +6,11 @@ import sys
 
 class Plugin:
 
+    channel_length = 0
     channelNames = []
     data = []
     tdm_model = {}
+    reader = None
 
     def read_store(self, parameter):
         file_path = os.path.realpath(parameter['file'])
@@ -18,6 +20,20 @@ class Plugin:
             reader = csv.DictReader(csvfile, delimiter=tab_delimiter)
             self.data = list(reader)
             self.channelNames = reader.fieldnames
+            self.channel_length = len(self.data)
+
+        values = {}
+
+        for row in self.data:
+            for col_name in self.channelNames:
+                value = row[col_name]
+                try:
+                    value = float(value) if value != "" else None
+                except:
+                    pass
+                if col_name not in values:
+                    values[col_name] = []
+                values[col_name].append(value)
 
         self.tdm_model['Example'] = {
             "author": "National Instruments",
@@ -26,7 +42,7 @@ class Plugin:
 
         ###
         # possible data types:
-        # DataTypeChnFloat32, DataTypeChnString, DataTypeChnDate, DataTypeChnUInt8, DataTypeChnInt16, DataTypeChnInt32, DataTypeChnInt64
+        # DataTypeChnFloat32, DataTypeChnFloat64, DataTypeChnString, DataTypeChnDate, DataTypeChnUInt8, DataTypeChnInt16, DataTypeChnInt32, DataTypeChnInt64
         ###
 
         group1 = {
@@ -36,57 +52,40 @@ class Plugin:
             "channels": [{
                 "name": self.channelNames[0],
                 "description": "",
-                "values": [],
+                "values": values[self.channelNames[0]],
                 "info": "Time in seconds",
                 "type": "DataTypeChnFloat64"
             }, {
                 "name": self.channelNames[1],
                 "description": "",
-                "values": [],
+                "values": values[self.channelNames[1]],
                 "unit_string": "km/h",
                 "type": "DataTypeChnFloat64"
             }, {
                 "name": self.channelNames[2],
                 "description": "",
-                "values": [],
+                "values": values[self.channelNames[2]],
                 "type": "DataTypeChnFloat64"
             }, {
                 "name": self.channelNames[3],
                 "description": "",
-                "values": [],
+                "values": values[self.channelNames[3]],
                 "type": "DataTypeChnFloat64"
             }, {
                 "name": self.channelNames[4],
                 "description": "",
-                "values": [],
+                "values": values[self.channelNames[4]],
                 "type": "DataTypeChnFloat64"
             }, {
                 "name": self.channelNames[5],
                 "description": "",
-                "values": [],
+                "values": values[self.channelNames[5]],
                 "type": "DataTypeChnString"
             }]
         }
 
         self.tdm_model['Example']['groups'].extend([group1])
         return self.tdm_model
-
-    def read_channel_length(self, grp_index, chn_index):
-        return len(self.data)
-
-    def read_channel_values(self, grp_index, chn_index, numberToSkip, numberToTake):
-        dataType = self.tdm_model['Example']['groups'][grp_index]['channels'][chn_index]['type']
-        values = []
-        for row in self.data:
-            value = row[self.channelNames[chn_index]]
-            if value != "":
-                if dataType == "DataTypeChnFloat64":
-                    value = float(row[self.channelNames[chn_index]])
-            else:
-                value = None
-            values.append(value)
-        return values[numberToSkip:numberToTake+numberToSkip]
-
 
 if __name__ == "__main__":
     print("For testing your plugin first, you can run that python file directly from command line")
@@ -95,5 +94,3 @@ if __name__ == "__main__":
         "file": "C:\\Users\\Public\\Documents\\National Instruments\\DIAdem 2020\\Data\\Example.csv"
     }
     print("\n %s" % p.read_store(parameter))
-    print("\nChannel length: %s" % p.read_channel_length(0, 0))
-    print("\nChannel values: %s" % p.read_channel_values(0, 0, 0, 1024))
