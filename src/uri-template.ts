96ffac949a9bb16986007f0ec346cd6e67f587b8
Jan Schummers <jan.schummers@ni.com>
2020-05-31 11:49:35 +0200

    linting fixed

--- src/uri-template.ts
+++ src/uri-template.ts
@@ -3,17 +3,17 @@ export class UriTemplate {
    private _templateString: string = '';
    constructor(fileExtensions: string, fileName: string, pythonScriptPath: string) {
       this._templateString = `<usireginfo><storetype name="${fileName}">` +
-         `<type>python</type>` +
+         '<type>python</type>' +
          `<alias>${fileName}</alias>` +
          `<description>${fileName}</description>` +
-         `<filepath>uspTdmMarshaller.dll</filepath>` +
-         `<exportsupported>NO</exportsupported>` +
-         `<caching>YES</caching>` +
+         '<filepath>uspTdmMarshaller.dll</filepath>' +
+         '<exportsupported>NO</exportsupported>' +
+         '<caching>YES</caching>' +
          `<easypluginparam><![CDATA[<dllpath>@USIBINDIR@\\PythonMarshaller\\uspPythonMarshaller.dll</dllpath><script>${pythonScriptPath}</script>]]></easypluginparam>` +
-         `<querysupported>0</querysupported>` +
-         `<fastloadsupported>0</fastloadsupported>` +
+         '<querysupported>0</querysupported>' +
+         '<fastloadsupported>0</fastloadsupported>' +
          `<filefilters extension="${fileExtensions}"><description>${fileName} Dateien (' + fileExtensions + ')</description></filefilters>` +
-         `<platform>x64</platform></storetype></usireginfo>`
+         '<platform>x64</platform></storetype></usireginfo>';
    }
 
    get templateString(): string {
