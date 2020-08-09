5bb3f142bcbef80f6dbff1b7396dbe34e5356d2e
Jan S <darkadmiral1337@gmail.com>
2020-08-09 16:10:27 +0200

    Add first basic e2e test

--- src/test/e2e/create-plugin.test.ts
+++ src/test/e2e/create-plugin.test.ts
@@ -1,27 +1,22 @@
 import * as assert from 'assert';
-import { Workbench, Notification, WebDriver, VSBrowser, NotificationType } from 'vscode-extension-tester';
+import { InputBox, Workbench, Notification, WebDriver, VSBrowser, NotificationType } from 'vscode-extension-tester';
 
-describe('Open VSCode', () => {
+/*
+   API Reference: https://github.com/redhat-developer/vscode-extension-tester/wiki/Page-Object-APIs
+*/
+
+describe('Basic UI Tests', () => {
    let driver: WebDriver;
 
    before(() => {
       driver = VSBrowser.instance.driver;
    });
 
-   it('Open VSCode', async () => {
+   it('Should show input box that waits for DataPlugin name to be entered', async () => {
       const workbench = new Workbench();
       await workbench.executeCommand('NI DataPlugins: Create new Python-DataPlugin');
-      // const notification = await driver.wait(() => { return notificationExists('Hello'); }, 2000) as Notification;
-      assert.ok(true);
+      const enterDataPluginNameInputBox = await driver.wait(() => { return new InputBox()});
+      const placeholderText = await enterDataPluginNameInputBox.getPlaceHolder();
+      assert.equal('Please enter your DataPlugin name', placeholderText);
    }).timeout(10000);
 });
-
-async function notificationExists(text: string): Promise<Notification | undefined> {
-   const notifications = await new Workbench().getNotifications();
-   for (const notification of notifications) {
-      const message = await notification.getMessage();
-      if (message.indexOf(text) >= 0) {
-         return notification;
-      }
-   }
-}
