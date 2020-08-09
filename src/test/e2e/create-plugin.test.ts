1a676626c9bb7a7932ac28740057845394712350
Jan Schummers <darkadmiral1337@gmail.com>
2020-08-09 16:14:48 +0200

    Add basic e2e testing (#12)
    * Add first basic e2e test

---
+++ src/test/e2e/create-plugin.test.ts
@@ -0,0 +1,22 @@
+import * as assert from 'assert';
+import { InputBox, Workbench, Notification, WebDriver, VSBrowser, NotificationType } from 'vscode-extension-tester';
+
+/*
+   API Reference: https://github.com/redhat-developer/vscode-extension-tester/wiki/Page-Object-APIs
+*/
+
+describe('Basic UI Tests', () => {
+   let driver: WebDriver;
+
+   before(() => {
+      driver = VSBrowser.instance.driver;
+   });
+
+   it('Should show input box that waits for DataPlugin name to be entered', async () => {
+      const workbench = new Workbench();
+      await workbench.executeCommand('NI DataPlugins: Create new Python-DataPlugin');
+      const enterDataPluginNameInputBox = await driver.wait(() => { return new InputBox()});
+      const placeholderText = await enterDataPluginNameInputBox.getPlaceHolder();
+      assert.equal('Please enter your DataPlugin name', placeholderText);
+   }).timeout(10000);
+});
