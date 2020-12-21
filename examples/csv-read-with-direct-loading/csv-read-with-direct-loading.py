9b9cbe4c69e0db5d66840ccd58ad66bb7a7a6ce4
Jan Schummers <jan.schummers@ni.com>
2020-12-21 16:59:48 +0100

    Better docs and examples (#18)
    * Added getting started doc
    
    * Known limitations added
    
    * TOC added
    
    * Better description of tdm structure
    
    * Example improved
    
    * Documentation improved
    
    * Error handling documented

--- examples/csv-read-with-direct-loading/csv-read-with-direct-loading.py
+++ examples/csv-read-with-direct-loading/csv-read-with-direct-loading.py
@@ -1,7 +1,7 @@
 import csv
 import datetime
 import os
-import sys
+from pathlib import Path
 
 
 class Plugin:
@@ -9,8 +9,7 @@ class Plugin:
     channel_length = 0
     channelNames = []
     data = []
-    tdm_model = {}
-    reader = None
+    tdm_tree = {}
 
     def read_store(self, parameter):
         file_path = os.path.realpath(parameter['file'])
@@ -35,57 +34,55 @@ class Plugin:
                     values[col_name] = []
                 values[col_name].append(value)
 
-        self.tdm_model['Example'] = {
-            "author": "National Instruments",
-            "description": "Example file"}
-        self.tdm_model['Example']['groups'] = []
-
         ###
         # possible data types:
         # DataTypeChnFloat32, DataTypeChnFloat64, DataTypeChnString, DataTypeChnDate, DataTypeChnUInt8, DataTypeChnInt16, DataTypeChnInt32, DataTypeChnInt64
         ###
 
-        group1 = {
-            "name": "Example",
-            "description": "The first group",
-            "time": datetime.datetime(2020, 2, 11, 15, 31, 59, 342380),
-            "channels": [{
-                "name": self.channelNames[0],
-                "description": "",
-                "values": values[self.channelNames[0]],
-                "info": "Time in seconds",
-                "type": "DataTypeChnFloat64"
-            }, {
-                "name": self.channelNames[1],
-                "description": "",
-                "values": values[self.channelNames[1]],
-                "unit_string": "km/h",
-                "type": "DataTypeChnFloat64"
-            }, {
-                "name": self.channelNames[2],
-                "description": "",
-                "values": values[self.channelNames[2]],
-                "type": "DataTypeChnFloat64"
-            }, {
-                "name": self.channelNames[3],
-                "description": "",
-                "values": values[self.channelNames[3]],
-                "type": "DataTypeChnFloat64"
-            }, {
-                "name": self.channelNames[4],
-                "description": "",
-                "values": values[self.channelNames[4]],
-                "type": "DataTypeChnFloat64"
-            }, {
-                "name": self.channelNames[5],
-                "description": "",
-                "values": values[self.channelNames[5]],
-                "type": "DataTypeChnString"
+        self.tdm_tree = {
+            "author": "National Instruments",
+            "description": "Example file",
+            "groups": [{
+                "name": "Example",
+                "description": "The first group",
+                "time": datetime.datetime(2020, 2, 11, 15, 31, 59, 342380),
+                "channels": [{
+                    "name": self.channelNames[0],
+                    "description": "",
+                    "values": values[self.channelNames[0]],
+                    "info": "Time in seconds",
+                    "type": "DataTypeChnFloat64"
+                }, {
+                    "name": self.channelNames[1],
+                    "description": "",
+                    "values": values[self.channelNames[1]],
+                    "unit_string": "km/h",
+                    "type": "DataTypeChnFloat64"
+                }, {
+                    "name": self.channelNames[2],
+                    "description": "",
+                    "values": values[self.channelNames[2]],
+                    "type": "DataTypeChnFloat64"
+                }, {
+                    "name": self.channelNames[3],
+                    "description": "",
+                    "values": values[self.channelNames[3]],
+                    "type": "DataTypeChnFloat64"
+                }, {
+                    "name": self.channelNames[4],
+                    "description": "",
+                    "values": values[self.channelNames[4]],
+                    "type": "DataTypeChnFloat64"
+                }, {
+                    "name": self.channelNames[5],
+                    "description": "",
+                    "values": values[self.channelNames[5]],
+                    "type": "DataTypeChnString"
+                }]
             }]
         }
 
-        self.tdm_model['Example']['groups'].extend([group1])
-        return self.tdm_model
+        return {Path(file_path).stem: self.tdm_tree}
 
 if __name__ == "__main__":
     print("For testing your plugin first, you can run that python file directly from command line")
--- examples/hello-world/hello-world.py
+++ examples/hello-world/hello-world.py
@@ -1,33 +1,62 @@
 import os
+from pathlib import Path
 
 
 class Plugin:
 
-    channel_length = 4
     tdm_model = {}
 
     def read_store(self, parameter):
-        file_path = os.path.realpath(parameter['file'])
+        file_path = os.path.realpath(parameter["file"])
 
-        self.tdm_model['HelloWorld'] = {
-            "author": "National Instruments",
-            "description": file_path
-        }
-        self.tdm_model['HelloWorld']['groups'] = []
-
-        group = {
-            "name": "TheFirstGroup",
-            "description": "The first group",
-            "channels": [{
-                "name": "TheFirstChannel",
-                "description": "The first channel",
-                "values": [1.0, 1.2, 1.4, 1.6],
-                "type": "DataTypeChnFloat64"
+        tdm_tree = {
+            "author": "HelloWorkd test",
+            "description": "File containing a json dict read by python plugin",
+            "groups": [{
+                "name": "Group_1",
+                "description": "The first group",
+                "channels": [{
+                    "name": "Index",
+                    "description": "",
+                    "info": "Going up",
+                    "unit_string": "s",
+                    "type": "DataTypeChnFloat64",
+                    "values": [1, 2, 3]
+                }, {
+                    "name": "Vals_1",
+                    "description": "",
+                    "unit_string": "km/h",
+                    "type": "DataTypeChnFloat64",
+                    "values": [1.1, 2.1, 3.1]
+                }, {
+                    "name": "Vals_2",
+                    "description": "",
+                    "unit_string": "km/h",
+                    "type": "DataTypeChnFloat64",
+                    "values": [1.2, 2.2, 3.2]
+                }, {
+                    "name": "Str_1",
+                    "description": "",
+                    "type": "DataTypeChnString",
+                    "values": ["abc", "def", "hij"]
+                }]
+            }, {
+                "name": "Group_2",
+                "description": "The first group",
+                "channels": [{
+                    "name": "Index",
+                    "description": "",
+                    "info": "Going up",
+                    "unit_string": "s",
+                    "type": "DataTypeChnFloat64",
+                    "values": [1, 2, 3, 4]
+                }
+                ]
             }]
         }
 
-        self.tdm_model['Example']['groups'].extend([group])
-        return self.tdm_model
+        return {Path(file_path).stem: tdm_tree}
+
 
 if __name__ == "__main__":
     print("For testing your plugin first, you can run that python file directly from command line")
