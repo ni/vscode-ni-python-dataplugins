184cf79dbc74e640c2541cdbf80e9d7b7c58fbdf
jschumme <jan.schummers@ni.com>
2020-12-22 15:34:02 +0100

    Implemented loading binary data

--- examples/npy-load/npy-load.py
+++ examples/npy-load/npy-load.py
@@ -1,5 +1,6 @@
 import os
 import re
+from array import array
 from pathlib import Path
 
 
@@ -9,109 +10,99 @@ class Plugin:
 
     def read_store(self, parameter):
         file_path = os.path.realpath(parameter["file"])
-
-        f = open(file_path, "r")
-        self.load_npy_file(f)
+        values = self.load_npy_file(file_path)
 
         tdm_tree = {
-            "author": "HelloWorkd test",
-            "description": "File containing a json dict read by python plugin",
+            "author": "National Instruments",
+            "description": "This file contains a numpy array",
             "groups": [{
                 "name": "Group_1",
                 "description": "The first group",
                 "channels": [{
                     "name": "Index",
                     "description": "",
-                    "info": "Going up",
-                    "unit_string": "s",
                     "type": "DataTypeChnFloat64",
-                    "values": [1, 2, 3]
-                }, {
-                    "name": "Vals_1",
-                    "description": "",
-                    "unit_string": "km/h",
-                    "type": "DataTypeChnFloat64",
-                    "values": [1.1, 2.1, 3.1]
-                }, {
-                    "name": "Vals_2",
-                    "description": "",
-                    "unit_string": "km/h",
-                    "type": "DataTypeChnFloat64",
-                    "values": [1.2, 2.2, 3.2]
-                }, {
-                    "name": "Str_1",
-                    "description": "",
-                    "type": "DataTypeChnString",
-                    "values": ["abc", "def", "hij"]
+                    "values": values
                 }]
-            }, {
-                "name": "Group_2",
-                "description": "The first group",
-                "channels": [{
-                    "name": "Index",
-                    "description": "",
-                    "info": "Going up",
-                    "unit_string": "s",
-                    "type": "DataTypeChnFloat64",
-                    "values": [1, 2, 3, 4]
-                }
-                ]
             }]
         }
 
         return {Path(file_path).stem: tdm_tree}
 
-    def load_npy_file(self, file):
-        print(file)
-        header = self.__read_npy_header(file)
-        print(header)
+    def load_npy_file(self, file_path):
+        f = open(file_path, errors='ignore')
+        header = self.__read_npy_header(f)
+        f.close()
+
+        shape = header['shape']
+
+        num_values = 1
+        for x in shape:
+            num_values *= x
+
+        f = open(file_path, 'rb')
+        next(f)  # skip first line
+
+        a = array(self.__map_datatype(header['datatype']))
+        a.frombytes(f.read())
+        f.close()
+
+        return a.tolist()
+
+    def __map_datatype(self, npy_data_type):
+        if npy_data_type == 'i4':
+            return 'i'
+        if npy_data_type == 'f8':
+            return 'd'
 
     def __read_npy_header(self, file):
         datafile = file.readlines()
 
+        datatype = None
         fortran_order = False
         little_endian = False
         shape = None
+        word_size = None
 
-        for line in datafile:
+        line = datafile[0]
 
-            loc_1 = line.find('fortran_order')
-            loc_2 = line.find('descr')
-            loc_3 = line.find('shape')
+        loc_1 = line.find('fortran_order')
+        loc_2 = line.find('descr')
+        loc_3 = line.find('shape')
 
-            # fortran order
-            if loc_1 > -1:
-                fortran_order = True if line[loc_1 +
-                                             16:loc_1+20] == 'True' else False
+        # fortran order
+        if loc_1 > -1:
+            fortran_order = True if line[loc_1 +
+                                         16:loc_1+20] == 'True' else False
 
-            # descr
-            if loc_2 > -1:
-                endian_char = line[loc_2+9:loc_2+10]
-                little_endian = True if (
-                    endian_char == '<' or endian_char == '|') else False
+        # descr
+        if loc_2 > -1:
+            endian_char = line[loc_2+9]
+            little_endian = True if (
+                endian_char == '<' or endian_char == '|') else False
+            sub_str = line[loc_2+11:]
+            loc_tmp = sub_str.find("'")
+            word_size = int(line[loc_2+11:loc_2+11+loc_tmp])
+            datatype = line[loc_2+10:loc_2+11+loc_tmp]
 
-            # shape
-            if loc_3 > -1:
-                start_loc = line.find('(')
-                end_loc = line.find(')')
+        # shape
+        if loc_3 > -1:
+            start_loc = line.find('(')
+            end_loc = line.find(')')
 
-                # finding first shape value
-                sub_str_1 = line[start_loc+1:end_loc]
-                match_1 = re.search("[0-9][0-9]*", sub_str_1)
-
-                # span tuple: start and end position of match
-                number_of_digits = match_1.span()[1] - match_1.span()[0]
-
-                # there must be a second match
-                sub_str_2 = line[start_loc+1+number_of_digits:end_loc]
-                match_2 = re.search("[0-9][0-9]*", sub_str_2)
-
-                shape = (int(match_1.group()), int(match_2.group()))
+            # finding shape value
+            shape = []
+            sub_str_1 = line[start_loc+1:end_loc]
+            pattern = re.compile(r'[0-9][0-9]*')
+            for number in re.findall(pattern, sub_str_1):
+                shape.append(int(number))
 
         header = {
+            'datatype': datatype,
             'fortran_order': fortran_order,
             'little_endian': little_endian,
-            'shape': shape
+            'shape': shape,
+            'word_size': word_size
         }
 
         return header
@@ -120,7 +111,9 @@ class Plugin:
 if __name__ == "__main__":
     print("For testing your plugin first, you can run that python file directly from command line")
     p = Plugin()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
     parameter = {
-        "file": "C:\\tmp\\data.npy"
+        "file": dir_path + "\\data.npy"
     }
+
     print("\n %s" % p.read_store(parameter))
