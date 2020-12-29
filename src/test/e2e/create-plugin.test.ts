dc86b8c173900a15f83f1ec6fc400c907bf7a704
jschumme <darkadmiral1337@gmail.com>
2020-12-29 15:03:54 +0100

    More work on e2e

--- src/test/e2e/create-plugin.test.ts
+++ src/test/e2e/create-plugin.test.ts
@@ -1,6 +1,4 @@
 import * as assert from 'assert';
-import * as fs from 'fs-extra';
-import * as path from 'path';
 import { Guid } from 'guid-typescript';
 import { InputBox, Workbench, WebDriver, VSBrowser, SideBarView } from 'vscode-extension-tester';
 
@@ -15,35 +13,35 @@ describe('Basic UI Tests', () => {
       driver = VSBrowser.instance.driver;
    });
 
-   // it('Should create a new DataPlugin project', async () => {
-   //    const randomName: string = Guid.create().toString();
-   //    await new Promise(res => setTimeout(res, 5000)); // wait for loading vscode completely
-   //    const workbench = new Workbench();
-   //    await workbench.executeCommand('NI DataPlugins: Create new Python-DataPlugin');
-   //    await new Promise(res => setTimeout(res, 500));
+   it('Should create a new DataPlugin project', async () => {
+      const randomName: string = Guid.create().toString();
+      await new Promise(res => setTimeout(res, 5000)); // wait for loading vscode completely
+      const workbench = new Workbench();
+      await workbench.executeCommand('NI DataPlugins: Create new Python-DataPlugin');
+      await new Promise(res => setTimeout(res, 500));
 
-   //    // Input box requesting a DataPlugin name?
-   //    const enterDataPluginNameInputBox = await driver.wait(() => { return new InputBox(); }, 1000);
-   //    const placeholderText1 = await enterDataPluginNameInputBox.getPlaceHolder();
-   //    assert.strictEqual('Please enter your DataPlugin name', placeholderText1);
-   //    await enterDataPluginNameInputBox.setText(randomName);
-   //    await enterDataPluginNameInputBox.confirm();
-   //    await new Promise(res => setTimeout(res, 500));
+      // Input box requesting a DataPlugin name?
+      const enterDataPluginNameInputBox = await driver.wait(() => { return new InputBox(); }, 1000);
+      const placeholderText1 = await enterDataPluginNameInputBox.getPlaceHolder();
+      assert.strictEqual('Please enter your DataPlugin name', placeholderText1);
+      await enterDataPluginNameInputBox.setText(randomName);
+      await enterDataPluginNameInputBox.confirm();
+      await new Promise(res => setTimeout(res, 500));
 
-   //    // Input box requesting a template?
-   //    const chooseTemplateDropDown = await driver.wait(() => { return new InputBox(); }, 1000);
-   //    const placeholderText2 = await chooseTemplateDropDown.getPlaceHolder();
-   //    assert.strictEqual('Please choose a template to start with', placeholderText2);
-   //    await new Promise(res => setTimeout(res, 500));
-   //    await chooseTemplateDropDown.setText('hello-world');
-   //    await chooseTemplateDropDown.confirm();
+      // Input box requesting a template?
+      const chooseTemplateDropDown = await driver.wait(() => { return new InputBox(); }, 1000);
+      const placeholderText2 = await chooseTemplateDropDown.getPlaceHolder();
+      assert.strictEqual('Please choose a template to start with', placeholderText2);
+      await new Promise(res => setTimeout(res, 500));
+      await chooseTemplateDropDown.setText('hello-world');
+      await chooseTemplateDropDown.confirm();
 
-   //    // Project with correct name created in SideBar?
-   //    const sideBarView = await driver.wait(() => { return new SideBarView(); }, 5000);
-   //    await new Promise(res => setTimeout(res, 5000));
-   //    const sideBarContent = await sideBarView.getContent().wait();
-   //    const section = await sideBarContent.getSection('Untitled (Workspace)');
-   //    const item = await section.findItem(randomName);
-   //    assert.notStrictEqual(undefined, item);
-   // }).timeout(40000);
+      // Project with correct name created in SideBar?
+      const sideBarView = await driver.wait(() => { return new SideBarView(); }, 5000);
+      await new Promise(res => setTimeout(res, 5000));
+      const sideBarContent = await sideBarView.getContent().wait();
+      const section = await sideBarContent.getSection('Untitled (Workspace)');
+      const item = await section.findItem(randomName);
+      assert.notStrictEqual(undefined, item);
+   }).timeout(40000);
 });
--- src/test/e2e/export-plugin.test.ts
+++ src/test/e2e/export-plugin.test.ts
@@ -1,46 +1,88 @@
 import * as assert from 'assert';
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { Guid } from 'guid-typescript';
-import { ActivityBar, InputBox, Workbench, WebDriver, VSBrowser, SideBarView } from 'vscode-extension-tester';
+import { ActivityBar, EditorView, InputBox, SideBarView, Workbench, WebDriver, VSBrowser } from 'vscode-extension-tester';
 
 /*
    API Reference: https://github.com/redhat-developer/vscode-extension-tester/wiki/Page-Object-APIs
 */
 
 describe('Basic UI Tests', () => {
+   let browser: VSBrowser;
    let driver: WebDriver;
 
    before(() => {
+      browser = VSBrowser.instance;
       driver = VSBrowser.instance.driver;
    });
 
    it('Should export a DataPlugin from a sample workspace', async () => {
       await new Promise(res => setTimeout(res, 5000)); // wait for loading vscode completely
-      
+
+      // Prepare Workspace
       const workspaceDir: string = path.join(__dirname, 'sample_workspace_1');
       fs.existsSync(workspaceDir) && fs.rmdirSync(workspaceDir, { recursive: true });
       fs.mkdirSync(workspaceDir);
-      fs.writeFileSync(path.join(workspaceDir, 'dataplugin.py'), 'class Python:')
+      fs.writeFileSync(path.join(workspaceDir, 'dataplugin.py'), 'class Plugin:')
 
+      // init
       const workbench = new Workbench();
+
+      // Open Workspace      
       await workbench.executeCommand('Extest: Open Folder');
       await new Promise(res => setTimeout(res, 500));
 
+      // Enter Workspace path
       const enterFolderName = await driver.wait(() => { return new InputBox(); }, 1000);
       await enterFolderName.setText(workspaceDir);
       await enterFolderName.confirm();
       await new Promise(res => setTimeout(res, 2000));
 
+      // Open Explorer side bar
       const sideBarView = new SideBarView();
-      const content = sideBarView.getContent();
-      const section = await content.getSection('Open Editors');
-      const item = section.findItem('dataplugin.py');
-      assert.notStrictEqual(undefined, item);
-      // content.findElement()
-      // const explorer = await new ActivityBar().getViewControl('Explorer');
-      // const view1 = await explorer.openView();
+      const explorer = await new ActivityBar().getViewControl('Explorer');
+      await explorer.openView();
 
+      // Find python file
+      const content = await sideBarView.getContent();
+      const section = await content.getSection('Untitled (Workspace)');
+      await section.openItem('sample_workspace_1');
+      const item = await section.findItem('dataplugin.py');
+      assert.notStrictEqual(undefined, item);
+      await new Promise(res => setTimeout(res, 2000));
+
+      // Ensure the remaining workflow is not disrupted by popping editor tabs from other extensions
+      const editorView = new EditorView();
+      await editorView.closeAllEditors();
+      await new Promise(res => setTimeout(res, 2000));
+
+      // Delete file extensions settings
+      const fileExtension = path.join(workspaceDir, '.file-extensions');
+      fs.existsSync(fileExtension) && fs.unlinkSync(fileExtension);
+
+      // Open ContextMenu and click Export
+      const menu = await item?.openContextMenu();
+      const menuItem = await menu?.getItem('NI DataPlugins: Export DataPlugin');
+      assert.notStrictEqual(undefined, menuItem);
+      await menuItem?.select();
+
+      // Choose File Extension
+      const fileExtensionInputBox = await driver.wait(() => { return new InputBox(); }, 1000);
+      const placeholderText = await fileExtensionInputBox.getPlaceHolder();
+      assert.strictEqual('*.tdm; *.xls ...', placeholderText);
+      await fileExtensionInputBox.setText('*.csv');
+      await fileExtensionInputBox.cancel();
+
+      // Write .file-extensions file      
+      fs.writeFileSync(fileExtension, '*.csv')
+
+      // Click Export again, this time no file extension prompt should show up
+      const menu2 = await item?.openContextMenu();
+      const menuItem2 = await menu2?.getItem('NI DataPlugins: Export DataPlugin');
+      await menuItem2?.select();
+      const checkForInputBoxAgain = await driver.wait(() => { return new InputBox(); }, 1000);
+      const isDisplayed = await checkForInputBoxAgain.isDisplayed();
+      assert.strictEqual(false, isDisplayed);
 
    }).timeout(40000);
 });
