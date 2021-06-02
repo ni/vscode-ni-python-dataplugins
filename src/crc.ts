312ecaa77a9e8977282fca92c77787cffd7c18b5
jschumme <jan.schummers@ni.com>
2021-06-02 10:59:55 +0200

    Merge eslint rules

--- src/crc.ts
+++ src/crc.ts
@@ -1,9 +1,11 @@
-import { crc32 } from "crc";
+import { crc32 } from 'crc';
+
 class CRC {
-  public static crc32(data: string): number {
-    let buffer = Buffer.from(data, "utf16le");
-    return crc32(buffer);
-  }
+    public static crc32(data: string): number {
+        const buffer = Buffer.from(data, 'utf16le');
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
+        return crc32(buffer);
+    }
 }
 
 export default CRC;
